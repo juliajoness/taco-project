4f06c2259a43e498c780491cdb5c6be991459ff3
Jennifer Man <jenniferman702@gmail.com>
2022-12-23 11:33:06 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :users
-  resources :tacos
+  resources :orders, only: [:index, :create]
+  resources :users, only: [:index, :show, :create]
+  resources :tacos, only: [:index, :show, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
