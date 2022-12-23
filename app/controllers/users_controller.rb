79f234edae004d77050796d21c5317c69f7e7f63
julia <juliajaj3@gmail.com>
2022-12-23 10:46:55 -0500

    create and reads finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    def index 
+        render json: User.all 
+    end
+
+    def show 
+        user_found = User.find_by(id: params[:id])
+        render json: user_found
+    end
+
+    def create
+        user_new = User.create(new_user_params)
+        if user_new.valid?
+            render json: user_new.order
+        else
+            render json: {"errors": "user not found"}
+        end
+    end
+
+    private
+
+    def new_user_params
+        params.permit(:time, :taco_id, :user_id)
+    end
 end
